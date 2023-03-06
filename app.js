4165727220147661f9255ec87a225c6c9728dc4f
Mahmoud Laban <mahmoud.laban@gmail.com>
2023-03-06 22:29:12 +0000

    created new app.js

--- app.js
+++ app.js
@@ -1,87 +1,51 @@
-// ℹ️ Gets access to environment variables/settings
-// https://www.npmjs.com/package/dotenv
+// Node.js application that uses the Express framework to handle HTTP requests
+
+// import the "dotenv" module to get access to environment variables, Dotenv is a zero-dependency module that loads environment variables from a .env file into process.env
 require("dotenv/config");
 
-// ℹ️ Connects to the database
+// import the ./db" module to connect to the database
 require("./db");
 
-// Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
+// import the express module
 const express = require("express");
 
-// Handles the handlebars
-// https://www.npmjs.com/package/hbs
+// install the handlebars module
 const hbs = require("hbs");
 
+//define app as an instance of express
 const app = express();
-//     |
-//     |-----------------------------|
-// use session here:                 V
-require("./config/session.config")(app);
-//                                  ^
-//                                  |
-// the "app" that gets passed here is the
-// previously defined Express app (const app = express();)
 
-// ℹ️ This function is getting exported from the config folder. It runs most middlewares
+// call the session configuration module and pass the "app" constant as an argument
+require("./config/session.config")(app);
+
+// call the module that runs the middlewares and pass the "app" constant as an argument 
 require("./config")(app);
 
-// default value for title local
-const projectName = "basic-auth";
-const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowerCase();
+////////////////////////////////////////////////////////////////////////////////
+//don't understand this yet
+// // default value for title local
+// const projectName = "basic-auth";
+// const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowerCase();
+// app.locals.title = `${capitalized(projectName)} created with Ironlauncher`;
+////////////////////////////////////////////////////////////////////////////////
 
-app.locals.title = `${capitalized(projectName)} created with Ironlauncher`;
-
-// 👇 Start handling routes here
+// define router for homepage endpoint authentication
 const index = require("./routes/index");
 app.use("/", index);
 
-
-const authRouter = require("./routes/auth.routes"); // <== has to be added
+// define router for authentication endpoint 
+const authRouter = require("./routes/auth.routes.js_old"); // <== has to be added
 app.use("/", authRouter); // <== has to be added
 
+// define router for products endpoint 
 const productRouter = require("./routes/product.routes"); // <== has to be added
 app.use("/", productRouter); // <== has to be added
 
+// define router for orders endpoint 
 const orderRouter = require("./routes/order.routes"); // <== has to be added
 app.use("/", orderRouter); // <== has to be added
 
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+// call the error handling module and pass the "app" constant as an argument
 require("./error-handling")(app);
 
 module.exports = app;
-
-
-
-
-/*
-
-    1st ITERATION
-    2-4PM
-
-    v 1st - Familiarize yourself with the HTML Code attached
-    v 2nd - Do the route for Get All Products
-    v 3rd - Do the View for Get All Products (this can be the user dashboard) - localStorage!
-    v 4th - Do the route/view for See Product (get product by Id)
-
-
-    2nd ITERATION:
-
-    v 1st - Implement Admin Dashboard -> Products List but with the Option to Change the product
-            Do the route for Get All Products Admin
-
-    v 2nd - Do the route/view for CHANGE Product (get product by Id and change it)
-    v 3rd - Create a different layout for signed out user, for user and for Admin
-
-
-    3rd ITERATION:
-
-    v 1st - Implement the Cart Page (A page that contains the Cart and the Products in it)
-    v 2nd - Implement the Checkout Page (A page that contains the elements and the Finish Purchase)
-    v 3rd - Finish Purchase adds an Order (implement that route)
-
-    BONUS:
-    > Page of Orders that can be Validated
-    > A route for the admin to validade an order
-
-*/
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
+//This module defines a simple route for the home page that renders a view using Handlebars. 
+//When a client navigates to the home page, the view engine will render the "index" template and send the resulting HTML to the client.
+
+//create a new router object and assign it to the "router" constant
 const router = require("express").Router();
 
-/* GET home page */
+//call the "router" object get() method to define a route for the home page "/"
+//The request handler function takes three arguments: req, res, and next. req contains information about the incoming request, res is the response object, and next is a function that can be called to pass control to the next middleware function in the chain.  The response handler function calls the res.render() method, which renders the index view and sends the HTML to the client.
 router.get("/", (req, res, next) => {
   res.render("index");
 });
