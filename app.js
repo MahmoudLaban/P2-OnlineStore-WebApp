fe2960285f46541c28e9333461bc0d0ad179d95e
BodyYvesEL <bodysoftwareengineer@gmail.com>
2023-03-18 13:22:13 +0100

    migrated

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config() // Loading environment variables from the .env file into the process.env.
+require('dotenv').config()
 const createError = require('http-errors')
 const express = require('express')
 const path = require('path')
@@ -9,21 +9,20 @@ const session = require('express-session')
 const passport = require('passport')
 const flash = require('connect-flash')
 const Category = require('./models/category')
-var MongoStore = require('connect-mongo')(session)
+const MongoStore = require('connect-mongo')(session)
 const connectDB = require('./config/db')
-const hbs = require('hbs') // Import the Handlebars.js view engine
+const hbs = require('hbs')
 
 const app = express()
-require('./config/passport') // Creating the Express.js app and import the Passport.js authentication configuration
+require('./config/passport')
 
-// Mongodb configuration....connect to MongoDB database
+// mongodb configuration
 connectDB()
-// View engine setup 
-// Configurating the view engine to use Handlebars.js and set the views directory to views + register the Handlebars.js partials.
+// view engine setup
 
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'view'))
 app.set('view engine', 'hbs')
-hbs.registerPartials(__dirname + '/views')
+hbs.registerPartials(__dirname + '/view')
 
 // Register custom helper function
 hbs.registerHelper('eq', function (a, b) {
@@ -70,48 +69,89 @@ hbs.registerHelper('lt', function(a, b) {
   return a < b;
 });
 
-// call the session configuration module and pass the "app" constant as an argument
-require("./config_old/session.config")(app);
+hbs.registerHelper('gt', function(a, b) {
+  return a > b;
+});
+hbs.registerHelper('lte', function (a, b, opts) {
+  if (a <= b) {
+    return opts.fn(this);
+  } else {
+    return opts.inverse(this);
+  }
+});
 
-// call the module that runs the middlewares and pass the "app" constant as an argument 
-require("./config_old/config")(app);
+hbs.registerHelper('gte', function (a, b, opts) {
+  if (a >= b) {
+    return opts.fn(this);
+  } else {
+    return opts.inverse(this);
+  }
+})
+hbs.registerHelper('Number', function(value) {
+  return Number(value);
+});
+hbs.registerHelper('if_eq', function(a, b, opts) {
+  if (a == b) {
+    return opts.fn(this);
+  } else {
+    return opts.inverse(this);
+  }
+});
+hbs.registerHelper('if_lt', function(a, b, opts) {
+  if (a < b) {
+    return opts.fn(this);
+  }
+});
+hbs.registerHelper('if_gt', function(a, b, opts) {
+  if (a > b) {
+    return opts.fn(this);
+  }
+});
+hbs.registerHelper('unless_eq', function(a, b, opts) {
+  if (a !== b) {
+    return opts.fn(this);
+  } else {
+    return opts.inverse(this);
+  }
+});
+// Define the slice helper function
+const slice = (arr, start, end) => arr.slice(start, end)
 
 // Register the slice helper with Handlebars
 hbs.registerHelper('slice', slice)
 
-// Admin route
-// Configurating Node.js/Express application by setting up various middleware
-const adminRouter = require('./routes/admin') // importing the adminRouter module from the file named admin.js located in ./routes...handling all routes responsible for handling all routes related to admin functionality.
-app.use('/admin', adminRouter) // sets up the adminRouter middleware to handle any requests that start with the path /admin.
+// admin route
+const adminRouter = require('./routes/admin')
+app.use('/admin', adminRouter)
 
-app.use(logger('dev')) // sets up the logger middleware with the 'dev' option to log HTTP requests and responses to the console in a colored format.
-app.use(express.json()) // sets up the express.json() middleware to parse incoming JSON data and make it available in the req.body object of the request object.
-app.use(express.urlencoded({ extended: false })) // sets up the express.urlencoded() middleware to parse incoming URL-encoded data and make it available in the req.body object of the request object.
-app.use(cookieParser()) // sets up the cookie-parser middleware to parse cookies from the HTTP request headers and make them available in the req.cookies object of the request object.
-app.use(express.static(path.join(__dirname, 'public'))) // sets up the express.static() middleware to serve static files such as images, stylesheets, and scripts from a directory named public located in the root directory of the application.
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     store: new MongoStore({
-      mongooseConnection: mongoose.connection, 
-    }), // sets up the express-session middleware to enable session handling. The middleware takes an object as its argument, which specifies the configuration options for the session handling. In this case, the session secret is obtained from an environment variable named SESSION_SECRET. The resave and saveUninitialized options are set to false to improve session storage efficiency. Finally, a MongoStore instance is created to store the session data in a MongoDB database using the mongoose connection.
-    //session expires after 3 hours... used to maintain the state of a user's session between requests.
-    cookie: { maxAge: 3 * 60 * 60 * 1000 },
+      mongooseConnection: mongoose.connection,
+    }),
+    //session expires after 3 hours
+    cookie: { maxAge: 60 * 1000 * 60 * 3 },
   }),
 )
-app.use(flash()) // using the flash middleware, which provides a way to store and retrieve flash messages in the session... e.g., your profile has been updated successfully.
-app.use(passport.initialize()) // initializing Passport, which is an authentication middleware for Node.js.
-app.use(passport.session()) // using the session middleware provided by Express to store Passport's user authentication state in the session.
+app.use(flash())
+app.use(passport.initialize())
+app.use(passport.session())
 
 // global variables across routes
-app.use(async (req, res, next) => { // defining a middleware function that will be executed on every request.
+app.use(async (req, res, next) => {
   try {
-    res.locals.login = req.isAuthenticated() // sets a login variable to true if the user is authenticated and false otherwise. 
-    res.locals.session = req.session // sets a session variable to the current session object. 
-    res.locals.currentUser = req.user // sets a currentUser variable to the currently authenticated user object (if any).
-    const categories = await Category.find({}).sort({ title: 1 }).exec() // retrieves all categories from the database and sorts them by title in ascending order.
+    res.locals.login = req.isAuthenticated()
+    res.locals.session = req.session
+    res.locals.currentUser = req.user
+    const categories = await Category.find({}).sort({ title: 1 }).exec()
     res.locals.categories = categories
     next()
   } catch (error) {
@@ -120,8 +160,8 @@ app.use(async (req, res, next) => { // defining a middleware function that will 
   }
 })
 
-// add breadcrumbs ... an array of objects, where each object represent a breadcrumb
-get_breadcrumbs = function (url) { // takes a URL as input, and returns an array of breadcrumb objects.
+// add breadcrumbs
+get_breadcrumbs = function (url) {
   var rtn = [{ name: 'Home', url: '/' }],
     acc = '', // accumulative url
     arr = url.substring(1).split('/')
@@ -135,20 +175,20 @@ get_breadcrumbs = function (url) { // takes a URL as input, and returns an array
   }
   return rtn
 }
-app.use(function (req, res, next) { // calls the get_breadcrumbs function with the req.originalUrl property, which contains the full original URL of the request.
+app.use(function (req, res, next) {
   req.breadcrumbs = get_breadcrumbs(req.originalUrl)
   next()
 })
 
-//Routes configurations
-const indexRouter = require('./routes/index') // imports the router for the root endpoint or index page of the application. This router will handle any requests made to the root URL of the application.
-const productsRouter = require('./routes/products') // imports the router for the products endpoint of the application. This router will handle any requests made to the /products URL of the application.
-const usersRouter = require('./routes/user') //  imports the router for the user endpoint of the application. This router will handle any requests made to the /user URL of the application.
-const pagesRouter = require('./routes/pages') // imports the router for the pages endpoint of the application. This router will handle any requests made to the /pages URL of the application.
-app.use('/products', productsRouter) // mounts the productsRouter middleware to the /products URL of the application. This means that any requests made to /products will be handled by the productsRouter.
-app.use('/user', usersRouter) // mounts the usersRouter middleware to the /user URL of the application. This means that any requests made to /user will be handled by the usersRouter.
-app.use('/pages', pagesRouter) // mounts the pagesRouter middleware to the /pages URL of the application. This means that any requests made to /pages will be handled by the pagesRouter.
-app.use('/', indexRouter) // mounts the indexRouter middleware to the root URL of the application. This means that any requests made to the root URL of the application will be handled by the indexRouter.
+//routes config
+const indexRouter = require('./routes/index')
+const productsRouter = require('./routes/products')
+const usersRouter = require('./routes/user')
+const pagesRouter = require('./routes/pages')
+app.use('/products', productsRouter)
+app.use('/user', usersRouter)
+app.use('/pages', pagesRouter)
+app.use('/', indexRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -166,7 +206,7 @@ app.use(function (err, req, res, next) {
   res.render('error')
 })
 
-const port = process.env.PORT || 3005
+const port = process.env.PORT || 3007
 app.set('port', port)
 app.listen(port, () => {
   console.log('Server running at port ' + port)
